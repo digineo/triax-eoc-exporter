92e4843c37a5ba10e400a9c7849b4d10d64f1880
Dominik Menke <dom@digineo.de>
2021-01-29 13:06:33 +0100

    import CI config from other projects

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.digineo.de/digineo/triax_eoc_exporter
 
 go 1.15
+
+require github.com/digineo/goldflags v0.0.0-20191122002131-f4bfb8d086f0
--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/debug"
 
+	"github.com/digineo/goldflags"
+)
+
+func main() {
+	fmt.Println(goldflags.Banner("Triax EoC Exporter"))
+	log.SetFlags(log.Lshortfile)
+
+	showVersion := flag.Bool("version", false, "print version information and exit")
+	flag.Parse()
+
+	if *showVersion {
+		printVersion()
+		os.Exit(0)
+	}
+}
+
+func printVersion() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	const l = "%-10s %-50s %s\n"
+	fmt.Println("Dependencies\n------------")
+	fmt.Printf(l, "main", info.Main.Path, info.Main.Version)
+	for _, i := range info.Deps {
+		if r := i.Replace; r != nil {
+			fmt.Printf(l, "dep", r.Path, r.Version)
+			fmt.Printf(l, "  replaces", i.Path, i.Version)
+		} else {
+			fmt.Printf(l, "dep", i.Path, i.Version)
+		}
+	}
 }
