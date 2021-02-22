81845f7f577f7cc2bdf4ce6b7da079fa3f9be3ea
Julian Kornberger <jk+github@digineo.de>
2021-02-22 18:48:01 +0100

    Allow different ports

--- exporter/config.go
+++ exporter/config.go
@@ -2,11 +2,15 @@ package exporter
 
 import (
 	"fmt"
+	"net"
 	"net/url"
+	"strconv"
 
 	"github.com/BurntSushi/toml"
 	"github.com/digineo/triax-eoc-exporter/triax"
 )
+
+const defaultPort = 443
 
 type Config struct {
 	// list of Triax EoC controllers
@@ -16,29 +20,44 @@ type Config struct {
 type Controller struct {
 	Alias    string
 	Host     string
+	Port     uint16
 	Password string
 }
 
+// LoadConfig loads the configuration from a file
 func LoadConfig(file string) (*Config, error) {
 	cfg := Config{}
 
 	if _, err := toml.DecodeFile(file, &cfg); err != nil {
 		return nil, fmt.Errorf("loading config file %q failed: %w", file, err)
 	}
+
 	return &cfg, nil
 }
 
+// getClient builds a client
 func (cfg *Config) getClient(target string) (*triax.Client, error) {
 	for _, ctrl := range cfg.Controllers {
 		if target == ctrl.Alias || target == ctrl.Host {
-			return triax.NewClient(&url.URL{
-				Scheme: "https",
-				User:   url.UserPassword("admin", ctrl.Password),
-				Host:   ctrl.Host,
-				Path:   "/",
-			})
+			return triax.NewClient(ctrl.url())
 		}
 	}
 
 	return nil, nil
+}
+
+// url build the URL
+func (ctrl *Controller) url() *url.URL {
+	host := ctrl.Host
+
+	if ctrl.Port > 0 {
+		host = net.JoinHostPort(host, strconv.Itoa(int(ctrl.Port)))
+	}
+
+	return &url.URL{
+		Scheme: "https",
+		User:   url.UserPassword("admin", ctrl.Password),
+		Host:   host,
+		Path:   "/",
+	}
 }
